08c64b8aef192df302800c97724242af25c7b7ee
Henry H. Andrews <andrews_henry@yahoo.com>
2023-07-26 11:09:37 -0700

    Fix merge error from subclassing merge

--- jschon/json.py
+++ jschon/json.py
@@ -478,13 +478,6 @@ class JSON(MutableSequence['JSON'], MutableMapping[str, 'JSON']):
 
 
 class CatalogedJSON(JSON):
-    _uri_cls: ClassVar[Type[URI]] = URI
-    """Associated :class:`URI` subclass for identification and loading.
-
-    This class is used for schema and vocabulary lookups in the
-    :class:`jschon.catalog.Catalog` and in keyword implementations.
-    """
-
     _catalog_cls: ClassVar[Type[Catalog]]
     """Associated :class:`Catalog` subclass for registering and loading."""
 
