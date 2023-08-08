ee9f9aa7c81c47dc6ca49d02fca4b60163e47b32
Henry H. Andrews <andrews_henry@yahoo.com>
2023-08-08 14:47:09 -0700

    checkpoint some rethinking

--- jschon/resource.py
+++ jschon/resource.py
@@ -11,125 +11,74 @@ if TYPE_CHECKING:
 class ResourceMixin:
     """Supports URIs and :class:`~jschon.catalog.Catalog` management.
 
-    Resources can serve as reference sources and / or targets, which
-    are resolved via the :class:`~jschon.catalog.Catalog` instance in
-    which they are cached.
+    This class faciliates the extension of ``jschon`` to formats
+    beyond JSON Schema, and is not intended for direct use.
 
-    The resource is cached under its :attr:`uri`; see
-    :meth:`init_resource` for how this is determined.
+    Resources have at least one URI and can serve as reference sources
+    and / or targets.  They can be loaded by requesting them by URI
+    from a :class:`~jschon.catalog.Catalog` instance, which is the
+    typical way to resolve references from one resource to another.
+
+    
+    are resolved via the :class:`~jschon.catalog.Catalog` instance in
+    which they are cached.  The resource is cached under its :attr:`uri`;
+    see :meth:`init_resource` for how this is determined.
+
+    In some document formats, only certain nodes are relevant to resource
+    identity.  This class supports the notion of a parent-in-resource,
+    which is the nearest parent node that might impact resource identity.
+    It also supports the determination of a resource root other than
+    the document root.
+
+    For example, in JSON Schema documents, only schema nodes can determine
+    resource identity, and only the `"$id"` keyword indicates a resource
+    root other than the document root.
+
+    A resource can encompass multiple formats, such as in OpenAPI 3.1
+    where a Schema Object is a JSON Schema-format structure, but is
+    not a distinct resource from the OpenAPI document unless it
+    contains `"$id"`.
     
     :class:`~jschon.json.JSON` subclasses representing document nodes
     in a URI-identified resource should inherit from this mixin and call
     :meth:`init_resource()` during construction.  Absolute-URIs (RFC 3986 §4.3)
     and URIs using fragment syntax other than JSON Pointer (RFC 6901)
-    are directly registered with the :class:`~jschon.catalog.Catalog`.
-
-    See the documentation for :meth:`init_resource`, :attr:`uri`,
-    :attr:`canonical_uri`, :attr:`base_uri`, and :attr:`url` for an
-    explanation of each possible URI and when and how it should be used.
+    should be directly registered with the :class:`~jschon.catalog.Catalog`.
 
     :class:`~jschon.json.JSON` subclasses that can contain references among
     or within documents must implement :meth:`resolve_references()`.
-
-
-    Resources can be identified by multiple URIs, which may each function as
-    identifiers (URIs), locators (URLs), or both.
-
-    A *canonical URI* (based on RFC 6596 "The Canonical Link Relation")
-    is the URI indicated by the document or its retrieval meta-data (e.g.
-    HTTP headers) as the preferred URI for accessing the resource.
-
-    As this mixin is inteded for nodes within a document, its
-    :
-    While RFC 6589 only discusses URIs / IRIs
-    for complete resources (without a URI fragment), this mixin is intended
-    for use with classes implementing nodes in a JSON document.  
-    absolute URIs (RFC 3986 §4.3
-
-    As this mixin is intended for use by nodes within a document tree,
-    the 
-
-    A URI functions as a URL if it is intended to be used to interact with
-    the resource by communicating with the URI's authority in accordance
-    with the URI's scheme.  The :class:`~jschon.catalog.Catalog` implements
-    a mapping between URIs and resources, which are retrieved through
-    a :class:`~jschon.catalog.Source`.
     """
     def init_resource(
         self,
-        catalog_uri: Optional[URI] = None,
-        content_uri: Optional[URI] = None,
-        source_url: Optional[URL] = None,
-        source_metadata: Optional[Dict] = None,
+        uri: Optional[URI] = None,
+        *,
         catalog: Union[Catalog, str] = 'catalog',
         cacheid: Hashable = 'default',
     ):
         """
         Configure identifiers and register with the catalog if needed.
 
-        Resources can have several URIs:
+        The resource is registered under the following conditions:
 
-        * a URI declare in content (JSON Schema's ``"$id"``)
-        * a URI used as a cache identifier in the catalog
-        * the URL from which the catalog actually loaded the contents
+        * ``uri`` does not have a fragment
+        * ``uri`` has an empty fragment, which is treated as a root
+          JSON Pointer; the resource is then registered under the 
+          semantically equivalent no-fragment URI
+        * ``uri`` has a non-JSON Pointer fragment
 
-        The catalog functions as a proxy and cache for retrieving resources,
-        so their cache key URI is considered the "request URI" for the
-        purposes of RFC 3986 §5.1.3.
+        The :class:`~jschon.catalog.Catalog` can resolve JSON Pointer fragment
+        URIs without cluttering the cache with them.
 
-        The source URL is intended only for use in debugging and error
-        reporting.  For example, the catalog key might represent a deployed
-        location, 
-        the catalog nor the one declared in 
-        * The URI used to request the resource from the catalog, which
-          should match the intended identity of the resource, such as
-          its production location.  
-          is the "retrieval URI" in RFC 3986 terms as the catalog
-          functions as a cache abstracting away the actual st
-        A the :class:`~jschon.catalog.Catalog` implements a cache that
-        abstracts the 
-        The resource is cached under its :attr:`uri`, which is the fully
-        resolved URI determined in the following order:
-
-        1. The ``catalog_uri`` parameter
-        2. The ``content_uri`` parameter
-        3. An auto-generated UUID URN
-
-        If provided, the ``initial_base_uri`` parameter is assumed to be
-        in compliance with RFC 3986 §5.1.2–5.1.4 for determinig a base
-        URI outside of the resource contents.  This can represent the
-        intended 
-        in the c
-        The ``source_url`` indicates how a resource was loaded into the
-        catalog's cache, if relevant.  It is intended for debugging and
-        error reporting purposes and is **not** considered the
-        "retrieval URI" for the purpose of establishing a base URI.
-        This is because the source location is considered an implementation
-        detail of the catalog's cache.
-
-        Since the :class:`~jschon.catalog.Catalog` functions as a cache,
-        even if backed by the local filesystem or a remote HTTPS site,
-        the ``source_url
-        :param catalog_uri: The URI under which this document is to be
-            registered with the :class:`~jschon.catalog.Catalog`.  If created
-            by the catalog, this will be the URI used to request the resource.
-            It is safe to re-register the resource with the catalog under
-            the same URI.
-        :param content_uri: The URI for this resource as defined
-            in its contents.  If no ``catalog_uri`` is passed, this is used
-            as the catalog cache key instead.
-        :param initial_base_uri: The base URI used to calculate the final base
-            URI if ``content_uri`` is relative; if no ``content_uri`` is
-            provided it can be the final base URI itself; see :attr:`base_uri`
-            for details.  Note that callers may opt to resolve a relative
-            ``content_uri`` prior to calling this method rather than passing
-            ``initial_base_uri``.
-        :param source_url:  The URL from which the document containing this
-            resource (which may be larger than the resource) was actually
-            loaded, if any.  This should be `None` if the resource was
-            instantiated from memory or if the calling code does not know
-            its source URL.
+        Resources can 
+        :param uri: The URI under which this document is to be registered
+            with the :class:`~jschon.catalog.Catalog`.  If created by the
+            catalog, this will be the URI used to request the resource.
+        :param catalog: The catalog name or instance managing this resource.
+        :param cacheid: The identifier for the cache within the catalog
+            in which this resource is stored.
         """
+
+
         try:
             parent = self.parent
             path = self.path
@@ -153,83 +102,58 @@ class ResourceMixin:
         if isinstance(catalog, str):
             from jschon.catalog import Catalog
             catalog = Catalog.get_catalog(catalog)
-        self._catalog = catalog
-        self._cacheid = cacheid
 
-        if catalog_uri is not None and not catalog_uri.has_absolute_base():
-            raise ValueError(
-                f"If provided, catalog_uri <{catalog_uri}> must begin "
-                "with a scheme!"
-            )
-
-        if content_uri is not None and not content_uri.has_absolute_base():
-            if catalog_uri is not None:
-                content_uri = content_uri.resolve(catalog_uri)
-            else:
-                raise ValueError(
-                    f"Cannot resolve relative URI reference
-                    <{content_uri}> without a base URI!"
-                )
-
-        if catalog_uri is None:
-            catalog_uri = content_uri if content_uri \
-                else URI(f'urn:uuid:{uuid4()}') 
-
-        frag = catalog_uri.fragment
-        if frag is None or (frag != '' and frag[0] != '/'):
-            catalog.add_schema(catalog_uri, self, cacheid=cacheid)
-        self._uri = catalog_uri
-
-        register = False
-        auto_uri = False
-        if catalog_uri is None:
-            catalog_uri = URI(f'urn:uuid:{uuid4()}') 
-            auto_uri = True
-            register = True
-        elif not catalog_uri.has_absolute_base():
-            raise ValueError(
-                f"{self.__class__.__name__} cannot use relative URI "
-                f"reference '{catalog_uri}` as a jschon.Catalog cache key!"
-            )
-        else:
-            # Don't register JSON Pointer fragment URIs
-            fragment = catalog_uri.fragment
-            if fragment is None or (
-                fragment != '' and not fragment.startswith('/')
-            ):
-                register = True
-
-        catalog.add_schema(catalog_uri, self, cacheid=cacheid)
-
+        self._catalog: Catalog = catalog
+        self._cacheid: Hashable = cacheid
         self._references_resolved: bool = False
-        self._uri: URI = if uri is None else uri
 
-        if root == self:
-            self._source_url: Optional[URI] = source_url
-            self._source_metadata: Dict = (
-                {} if source_metadata is None else source_metadata
-            )
-        elif (
-            (source_url is not None and source_url != root._source_url) or
-            (
-                source_metadata is not None and
-                source_metadata != root._source_metadata
-            )
-        ):
-            raise ValueError(
-                f"Source URL '{source_url}' or metadata conflicts with "
-                f"root source URL '{root._source_uri}' or metadata!"
-            )
-        else:
-            self._source_url = None
-            self._source_metadata = {}
+        # TODO: Optional
+        self._uri: Optional[URI] = (
+            URI(f'urn:uuid:{uuid4()}')
+            if uri is None and self.parent is None
+            else uri
+        )
+
+        if not self._uri.has_absolute_base():
+            raise ValueError(f"Relative URI-reference not allowed!")
+
+        frag = self._uri.fragment
+        if frag in (None, '') or (frag[0] != '/'):
+            # Add all non-JSON Pointer fragment URIs.  Strip the
+            # empty JSON Pointer if relevant.
+            if frag == '':
+                self._uri = self._uri.copy(fragment=None)
+            catalog.add_resource(self._uri, self, cacheid=cacheid)
 
     @property
-    def parent(self):
-        raise NotImplementedError
+    def parent_in_resource(self) -> ResourceMixin:
+        """Returns the nearest parent node that is of a resource type, if any.
+
+        The parent-in-resource may be of a different 
+        """
+        candidate = None
+        while next_candidate := self.parent:
+            if isinstance(next_candidate, ResourceMixin):
+                candidate = next_candidate
+        return candidate
 
     @property
-    def uri(self):
+    def resource_root(self) -> ResourceMixin:
+        candidate = self
+        while next_candidate := self.parent_in_resource:
+            if next_candidate is not None:
+                candidate = next_candidate
+        return candidate
+
+    def is_resource_root(self) -> bool:
+        """True if no parent in the document is part of this same resource.
+
+        Classes for documents that can contain multiple resources need to override this.
+        """
+        return self.parent_in_resource is None
+
+    @property
+    def uri(self) -> URI:
         """The URI identifying the resource in the catalog's cache.
 
         If instantiated through a :class:`Catalog`, this should be the URI by
@@ -237,22 +161,22 @@ class ResourceMixin:
         in the document (e.g. by ``"$id"``) should be used.  If no such URI
         exists, then a UUID URN is automatically generated by this mixin.
         """
-        return self._uri
+        return self._uri if self._uri else self.pointer_uri
 
     @property
-    def canonical_uri(self):
-        """The preferred URI for identifying this node in the resource.
+    def pointer_uri(self):
+        """The URI of this node using its base URI and a JSON Pointer fragment.
 
-        This concept is based on RFC 6596 "The Canonical Link Relation".
-        While that RFC does not address URI fragments, since this mixin
-        is used to implement nodes in a JSON document tree, this attribute
-        always includes the JSON Pointer fragment for the node.
-
-        If the resource declares a URI in its content, this URI will
-        always have the same :attr:`base_uri` as that URI, plus the
-        appropriate JSON Pointer fragment.  This may be different from
-        :attr:`uri`.
+        This property is similar to JSON Schema's "canonical URI", but
+        is unambiguous and consistent with respect to fragments.
         """
+        if self._uri:
+            fragment = self._uri.fragment
+            if fragment == '' or fragment[0] == '/':
+                return self._uri
+            if fragment is None:
+                return self._uri.copy(fragment='')
+        
         return self._canonical_uri
 
     @property
