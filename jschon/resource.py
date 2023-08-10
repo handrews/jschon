5ced1d26c719608f5888502ad69eaaf5dcdd0577
Henry H. Andrews <andrews_henry@yahoo.com>
2023-08-09 17:37:43 -0700

    loads

--- jschon/resource.py
+++ jschon/resource.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from uuid import uuid4
 
-from typing import Any, ContextManager, Dict, Hashable, Iterator, Mapping, Optional, TYPE_CHECKING, Tuple, Type, Union
+from typing import Any, ContextManager, Dict, FrozenSet, Hashable, Iterator, Mapping, Optional, Set, TYPE_CHECKING, Tuple, Type, Union
 
 from jschon import JSON, URI
 from jschon.exc import JschonError
@@ -31,16 +31,21 @@ class ResourceNotReadyError(ResourceError):
 class ResourceURINotSetError(ResourceError):
     """Raised when accessing any URI property before the URI has been set."""
 
+
 class RelativeResourceURIError(ResourceError):
     """Raised when attempting to set a URI to a relative URI-reference without an available base URI."""
+
+
+class BaseURIConflictError(ResourceError):
+    """Raised when attempting to set a URI in a way that conflicts with the established base URI for the resource."""
 
 
 @dataclass(frozen=True)
 class ResourceURIs:
     """Data structure for organizing URIs by use case."""
 
-    catalog_uri: Optional[URI]
-    """The URI to use as a :class:`~jschon.catalog.Catalog key, if any."""
+    register_uri: bool
+    """Indicates if :attr:`property_uri` should be registered with the catalog."""
 
     property_uri: URI
     """The URI to return from the :attr:`uri` property."""
@@ -50,6 +55,9 @@ class ResourceURIs:
 
     base_uri: URI
     """The base URI for the resource."""
+
+    additional_uris: Set[URI]
+    """Alternate URIs to register with the catalog."""
 
 
 class JSONResource(JSON):
@@ -84,6 +92,28 @@ class JSONResource(JSON):
 
     :class:`~jschon.json.JSON` subclasses that can contain references among
     or within documents must implement :meth:`resolve_references()`.
+
+    Parameters not documented here are passed directly to the
+    :class:`~jschon.json.JSON` constructor.
+
+    :param catalog: The catalog name or instance managing this resource.
+    :param cacheid: The identifier for the cache within the catalog
+        in which this resource is stored.
+    :param uri: The URI initially used to identify the resource.  If being
+        instantiated directly, this is the URI (if any) assigned by the
+        caller, and will be used ot determine the
+        :class:`~jschon.catalog.Catalog` registration URI(s).  If being
+        instantiated by the :class:`~jschon.catalog.Catalog`, this is
+        the URI provided to the :meth:`~jschon.catalog.Catalog.get_resource`
+        call (or any type-specific version of it, e.g. ``get_schema()``).
+        See :meth:`uris_for` for how various cases, including no URI
+        being provided, are handled.
+    :param additional_uris: Additional URIs under which this resource
+        should be registered.  The caller is responsible for ensuring
+        that these URIs, along with the ``uri`` parameter, do not
+        conflict with each other in problematic ways.  URIs with non-empty
+        JSON Pointer fragments are ignored; see :meth:`uris_for` for an
+        explanation.
     """
     def __init__(
         self,
@@ -94,6 +124,7 @@ class JSONResource(JSON):
         catalog: Union[str, Catalog] = 'catalog',
         cacheid: Hashable = 'default',
         uri: Optional[URI] = None,
+        additional_uris: Set[URI] = frozenset(),
         resolve_references: bool = True,
         itemclass: Type[JSON] = None,
         **itemkwargs: Any,
@@ -105,6 +136,12 @@ class JSONResource(JSON):
             parent=parent,
             key=key,
             itemclass=itemclass,
+            # The remaing args are received by JSON in **itemkwargs
+            catalog=catalog,
+            cacheid=cacheid,
+            uri=uri,
+            additional_uris=frozenset(),
+            resolve_references=resolve_references,
             **itemkwargs,
         )
         # self.init_resource(uri, catalog=catalog, cacheid=cacheid)
@@ -115,6 +152,7 @@ class JSONResource(JSON):
         self,
         *args,
         uri: Optional[URI] = None,
+        additional_uris: Sequence[URI] = frozenset(),
         catalog: Union[Catalog, str] = 'catalog',
         cacheid: Hashable = 'default',
         **kwargs,
@@ -145,8 +183,6 @@ class JSONResource(JSON):
         :param cacheid: The identifier for the cache within the catalog
             in which this resource is stored.
         """
-
-
         try:
             self.parent, self.path, self.document_root, self.resource_root
         except AttributeError:
@@ -157,8 +193,9 @@ class JSONResource(JSON):
             catalog = Catalog.get_catalog(catalog)
 
         self._uri: Optional[URI] = None
-        self._catalog_uri: Optional[URI] = None
+        self._pointer_uri: Optional[URI] = None
         self._base_uri: Optional[URI] = None
+        self._additional_uris: FrozenSet[URI] = frozenset()
 
         self.catalog: Catalog = catalog
         self.cacheid: Hashable = cacheid
@@ -181,9 +218,9 @@ class JSONResource(JSON):
             catalog.add_resource(self._uri, self, cacheid=cacheid)
 
     def _get_pointer_uri(self):
-        root = self.resource_root
-        ptr_from_root = self.path[len(root.path):]
-        return root.uri.copy(fragment=ptr_from_root.uri_fragment())
+        res_root = self.resource_root
+        ptr_from_res_root = self.path[len(root.path):]
+        return root.uri.copy(fragment=ptr_from_res_root.uri_fragment())
 
     def uris_for(self, uri: Optional[URI]) -> ResourceURIs:
         """Determine the URIs for various use cases.
@@ -205,62 +242,70 @@ class JSONResource(JSON):
             if self.is_resource_root():
                 urn = URI(f'urn:uuid:{uuid4()}')
                 return ResourceURIs(
-                    catalog_uri=urn,
+                    register_uri=True,
                     property_uri=urn,
                     pointer_uri=urn.copy(fragment=''),
                     base_uri=urn,
+                    additional_uris=set(),
                 )
 
             ptr_uri = self._get_pointer_uri()
             return ResourceURIs(
-                catalog_uri=None,
+                register_uri=False,
                 property_uri=ptr_uri,
                 pointer_uri=ptr_uri,
-                base_uri=ptr_uri.copy(fragment=None),
+                base_uri=self.resource_root.base_uri,
+                additional_uris=set(),
             )
 
         fragment = uri.fragment
         if fragment is None:
             return ResourceURIs(
-                catalog_uri=uri,
+                register_uri=True,
                 property_uri=uri,
                 pointer_uri=uri.copy(fragment=''),
                 base_uri=uri,
+                additional_uris=set(),
             )
 
         if fragment == '':
             absolute_uri = uri.copy(fragment=None)
             return ResourceURIs(
-                catalog_uri=absolute_uri,
+                register_uri=True,
                 property_uri=absolute_uri,
                 pointer_uri=uri,
                 base_uri=absolute_uri,
+                additional_uris=set(),
             )
 
         if fragment[0] == '/':
             return ResourceURIs(
-                catalog_uri=None,
+                register_uri=False,
                 property_uri=uri,
                 pointer_uri=uri,
                 base_uri=uri.copy(fragment=None),
+                additional_uris=set(),
             )
 
-        # Non-JSON Pointer fragment
+        # Non-JSON Pointer fragment at root node
         if self.is_resource_root():
             absolute = uri.copy(fragment=None)
             return ResourceURIs(
-                catalog_uri=absolute,
-                property_uri=uri,
+                register_uri=True,
+                property_uri=absolute,
                 pointer_uri=uri.copy(fragment=''),
                 base_uri=absolute,
+                additional_uris={uri},
             )
 
+        # Non-JSON Pointer fragment at sub-document node
         ptr_uri = self._get_pointer_uri()
         return ResourceURIs(
-            catalog_uri=uri,
+            register_uri=True,
             property_uri=uri,
             pointer_uri=ptr_uri,
             base_uri=ptr_uri.copy(fragment=None),
+            additional_uris=set(),
         )
 
     @property
@@ -299,16 +344,38 @@ class JSONResource(JSON):
         """
         return self.parent_in_resource is None
 
+    # TODO: This assumes that a node will know whether it is a resource root
+    #       before setting a new absolute-URI (and therefore base URI).
+    #       Is this correct, or should setting such a URI indicate that
+    #       the node is a resource root?  We probably do not wnat to rely
+    #       on callers always setting absolute-URIs correctly to determine
+    #       resource-rootness.
     @property
     def uri(self) -> URI:
         """The URI identifying the resource in the catalog's cache.
 
-        Note that if this URI has a JSON Pointer fragment, the catalog
-        cache will only explicitly contain the :attr:`base_uri`.
+        While :attr:`pointer_uri` gives the location of this node in
+        the JSON structure of the resource, this attribute is intended
+        to be externally-facing when relevant.  While the code does not
+        enforce this, callers are recommended to prefer:
 
-        In come cases, a resource may be cached under multiple URIs.
-        This URI is based on the one passed to :meth:`init_uri` or set
-        through this attribute.
+        * the :attr:`base_uri` if this is node is the resource root
+        * a URI with a non-JSON Pointer fragment attached to :attr:`base_uri`
+        * the :attr:`pointer_uri`
+
+        If this node is the :attr:`resource_root`, the base URI can be
+        changed by setting this property ao an absolute-URI, or to a
+        URI with a non-JSON Pointer fragment (which will set :attr:`base_uri`
+        to that URI's base).  Setting a non-resource-root URI that
+        disagrees with :attr:`base_uri` is not allowed; see
+        :attr:`additional_uris` for managing alternate URI registration.
+
+        See :meth"`uris_for` for an explanation of how JSON Pointer fragment
+        URIs are handled as catalog cache keyes.
+
+        Assigning to this URI will unregister the old URI from the catalog
+        and re-register this resource under the new URI, and update
+        :attr:`base_uri` and :attr:`pointer_uri` if appropriate.
         """
         if self._uri is None:
             raise ResourceURINotSetError()
@@ -316,24 +383,20 @@ class JSONResource(JSON):
 
     @uri.setter
     def uri(self, uri: Optional[URI]) -> None:
-        """Set all URI properties from an input URI; see :meth:`uris_for`."""
         uris = self.uris_for(uri)
+        if self.is_resource_root:
+            if self._base_uri is not None and self.base_uri != uris.base_uri:
+                # TODO: update children
+                #       how to handle children with non-JSON Pointer URIs?
+                raise NotImplementedError
+        elif self.base_uri != uris.base_uri:
+            raise BaseURIConflictError()
 
-        if self._catalog_uri is None:
-            self._catalog_uri = uris.catalog_uri
+        if uris.register_uri:
+            if self._uri is not None and self._uri != uris.property_uri:
+                self.catalog.del_resource(self._uri, cacheid=self.cacheid)
             self.catalog.add_resource(
-                self._catalog_uri,
-                self,
-                cacheid=self.cacheid,
-            )
-        elif self._catalog_uri != uris.catalog_uri:
-            self.catalog.del_resource(
-                self._catalog_uri,
-                cacheid=self.cacheid,
-            )
-            self._catalog_uri = uris.catlog_uri
-            self.catalog.add_resource(
-                self._catalog_uri,
+                uris.property_uri,
                 self,
                 cacheid=self.cacheid,
             )
@@ -341,6 +404,55 @@ class JSONResource(JSON):
         self._uri = uris.property_uri
         self._pointer_uri = uris.pointer_uri
         self._base_uri = uris.base_uri
+
+        # TODO: Might we ever want to unregister existing additional URIs?
+        self.additional_uris = self.additional_uris | uris.additional_uris
+
+    @property
+    def additional_uris(self) -> FrozenSet[URI]:
+        """URIs other than :attr:`uri` registered with the catalog.
+
+        This set is intended to be used for non-JSON Pointer-fragment
+        URIs such as plain name fragments and alternate absolute-URIs
+        under which this resource might be requested, none of which
+        affect the resource's base URI or are automatically propagated
+        to child resource nodes.
+
+        URIs in this set with an empty JSON Pointer fragment are converted
+        to the absolute-URI form, which is semantically equivalent.
+
+        If :attr:`uri` (or its empty JSON Pointer fragment equivalent)
+        is in the set assigned to this property, it will be ignored.
+
+        To change :attr:`base_uri` and :attr:`pointer_uri`, assign to
+        :attr:`uri` rather thian this attribute.
+        
+        This set is not guaranteed to be complete, as external code
+        can add resources to catalogs directly.
+
+        Setting this attribute will unregister removed URIs and register
+        the added ones.  URIs with non-empty JSON Pointer fragments are
+        dropped from this set for reasons described in the documentation
+        for :meth:`uri-for`.  An empty JSON Pointer fragment will be stripped,
+        and the result used (unless it is identical to :attr:`uri`, in which
+        case it wil be ignored(.
+        """
+        return self._additional_uris
+
+    @additional_uris.setter
+    def additional_uris(self, uris: Set[URI]) -> None:
+        uris = {
+            u.copy(fragment=None) if u.fragment == '' else u
+            for u in uris
+            if u.fragment in (None, '') or u.fragment[0] != '/'
+        }
+        uris.discard(self.uri)
+        for removed in self._additional_uris - uris:
+            self.catalog.del_resource(removed, cacheid=self.cacheid)
+        for added in uris - self._additional_uris:
+            self.catalog.add_resource(added, self, cacheid=self.cacheid)
+
+        self._additional_uris = frozenset(uris)
 
     @property
     def pointer_uri(self) -> URI:
